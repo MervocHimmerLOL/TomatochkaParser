4391ba4f54506fa1940921ca1415dc02aba1d113
Михаил Меренцов <flash00052@gmail.com>
2025-03-28 11:58:49 +0300

    Combined parser and sql logic + refactored a bit

--- parser.py
+++ parser.py
@@ -1,17 +1,21 @@
 import time
+import datetime
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from sql import engine, metadata_obj, create_table_city, insert_beer, csv_it
+import dateparser
 
-def find_beer():
-    if beer_name in name:
-        print(name)
-        name_element = beer.find(class_='productItem__name')
+
+def find_beer(beer_title, beer_element):
+    if beer_name.lower() in beer_title.lower():
+        name_element = beer_element.find(class_='productItem__name')
         href_value = name_element.find_parent('a')['href']
-        driver.get(url+href_value[1:])
+        driver.get(url + href_value[1:])
 
         # Ожидание появления селектора города
         select = WebDriverWait(driver, 10).until(
@@ -23,8 +27,8 @@ def find_beer():
         except Exception as e:
             # Задаем значения по умолчанию для adr и date
             adr = "Адрес не доступен"
-            date = "Дата не доступна"
-            print(adr, date)
+            date = datetime.date(1970, 1, 1)
+            insert_beer(city_table, name, adr, date)
 
         # Пауза для загрузки данных
         time.sleep(0.5)
@@ -38,16 +42,20 @@ def find_beer():
         adr = soup.find_all(class_='partnerItem__adr')
         date = soup.find_all(class_='partnerItem__order')
         for i in range(len(date)):
-            print(place[i].text, adr[i].text, date[i].text)
+            cleaned_text = date[i].text.strip().replace("Последний раз пиво отправлялось сюда:", "").strip()
+            date_parsed = dateparser.parse(cleaned_text, languages=['ru'])
+            insert_beer(city_table, name, place[i].text + " " + adr[i].text, date_parsed.date())
         time.sleep(3)
 
+start_time = time.time()
 # Инициализация драйвера
 driver = webdriver.Chrome()
 
 # Параметры поиска
-beer_name = 'Зависимость'
+beer_name = 'зависимость'
 city = "Белгород"
 
+city_table = create_table_city(city)
 
 # URL сайта
 url = 'https://4brewers.ru/'
@@ -62,7 +70,10 @@ beers = soup.find_all(class_='productItem')
 
 for beer in beers:
     name = beer.find(class_='productItem__name').text.strip()
-    find_beer()
+    find_beer(name, beer)
 
+# csv_it(city_table)
 # Закрытие драйвера
 driver.quit()
+end_time = time.time()
+print(f'Выполнили код за {end_time - start_time:.2f} секунд')
--- sql.py
+++ sql.py
@@ -1,11 +1,14 @@
-from sqlalchemy import create_engine
+from datetime import datetime
+
+from sqlalchemy import create_engine, UniqueConstraint
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Date
 from sqlalchemy import insert
 import csv
 
 engine = create_engine("sqlite+pysqlite:///beer.db", echo=True)
 metadata_obj = MetaData()
+
 
 def create_table_city(city):
     beer_table = Table(
@@ -14,15 +17,19 @@ def create_table_city(city):
         Column('id', Integer, primary_key=True),
         Column('name', String(60)),
         Column('address', String(60)),
-        Column('last_arr_time', String(60))
+        Column('last_arr_time', Date),
+        Column('cur_date', Date),
+        UniqueConstraint('name', 'address', 'last_arr_time', 'cur_date')
     )
     metadata_obj.create_all(engine)
     return beer_table
 
-def insert_beer(beer_table, beer_name, beer_adr, beer_arr_time):
-    cmd = insert(beer_table).values(name=beer_name, address=beer_adr, last_arr_time=beer_arr_time)
+
+def insert_beer(beer_table, beer_name, beer_adr, beer_arr_time, cur_date=datetime.today()):
+    cmd = insert(beer_table).prefix_with('OR IGNORE').values(name=beer_name, address=beer_adr,
+                                                             last_arr_time=beer_arr_time, cur_date=cur_date)
     with engine.connect() as conn:
-        res = conn.execute(cmd)
+        conn.execute(cmd)
         conn.commit()
 
 
