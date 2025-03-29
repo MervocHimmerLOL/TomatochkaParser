57c4ef85c32189d0f646706d76fc905500e8ae9e
Михаил Меренцов <flash00052@gmail.com>
2025-03-30 00:24:55 +0300

    Major refactor + bugfix + combined sql and parser logic

--- parser.py
+++ parser.py
@@ -1,79 +1,101 @@
 import time
 import datetime
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from sql import engine, metadata_obj, create_table_city, insert_beer, csv_it
+from selenium.webdriver.support.wait import WebDriverWait
+from sql import create_table_city, insert_beer, csv_it
 import dateparser
 
 
-def find_beer(beer_title, beer_element):
-    if beer_name.lower() in beer_title.lower():
-        name_element = beer_element.find(class_='productItem__name')
-        href_value = name_element.find_parent('a')['href']
-        driver.get(url + href_value[1:])
+class Parse4Brewers:
+    def __init__(self, city, beer_name=''):
+        self.driver = webdriver.Chrome()
+        self.url = 'https://4brewers.ru/'
+        self.city = city
+        self.city_table = create_table_city(city)
+        self.beer_name = beer_name
 
-        # Ожидание появления селектора города
-        select = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'city__selector'))
-        )
-        city_selector = Select(select)
-        try:
-            city_selector.select_by_visible_text(city)
-        except Exception as e:
-            # Задаем значения по умолчанию для adr и date
-            adr = "Адрес не доступен"
-            date = datetime.date(1970, 1, 1)
-            insert_beer(city_table, name, adr, date)
+    def get_soup(self):
+        page_source = self.driver.page_source
+        return BeautifulSoup(page_source, 'html.parser')
 
-        # Пауза для загрузки данных
-        time.sleep(0.5)
+    def process_page(self, beer_title, beer_element):
+        def process_beer(soup_object, city_table_object, beer_name, beer_sort):
+            partners = soup_object.find_all(class_='partnerItem')
+            for partner in partners:
+                beer_place = partner.find_all(class_='partnerItem__name')
+                beer_adr = partner.find_all(class_='partnerItem__adr')
+                beer_date = partner.find_all(class_='partnerItem__order')
+                for i in range(len(beer_place)):
+                    cleaned_text = beer_date[i].text.strip().replace("Последний раз пиво отправлялось сюда:","").strip()
+                    date_parsed = dateparser.parse(cleaned_text, languages=['ru'])
+                    for address in beer_adr:
+                        insert_beer(city_table_object, beer_name, beer_place[i].text + " " + address.text, date_parsed.date(), beer_sort)
 
-        # Получение HTML-кода страницы после выбора города
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
+        def get_beer_url(beer_element_to_get_href):
+            name_element = beer_element_to_get_href.find(class_='productItem__name')
+            href_value = name_element.find_parent('a')['href']
+            return self.url + href_value[1:]
 
-        # Извлечение данных с помощью BeautifulSoup
-        place = soup.find_all(class_='partnerItem__name')
-        adr = soup.find_all(class_='partnerItem__adr')
-        date = soup.find_all(class_='partnerItem__order')
-        for i in range(len(date)):
-            cleaned_text = date[i].text.strip().replace("Последний раз пиво отправлялось сюда:", "").strip()
-            date_parsed = dateparser.parse(cleaned_text, languages=['ru'])
-            insert_beer(city_table, name, place[i].text + " " + adr[i].text, date_parsed.date())
-        time.sleep(3)
+        if self.beer_name.lower() in beer_title.lower():
+            # Получаем ссылку на сорт
+            sort = get_beer_url(beer_element)
+            self.driver.get(sort)
 
-start_time = time.time()
-# Инициализация драйвера
-driver = webdriver.Chrome()
+            # Ожидание появления селектора города
+            select = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'city__selector'))
+            )
+            city_selector = Select(select)
 
-# Параметры поиска
-beer_name = 'зависимость'
-city = "Белгород"
+            # Если нет города, то вписываем с дефолтными значениями
+            try:
+                city_selector.select_by_visible_text(self.city)
+            except Exception as e:
+                # Задаем значения по умолчанию для adr и date
+                adr = "Адрес не доступен"
+                date = datetime.date(1970, 1, 1)
+                insert_beer(self.city_table, beer_title, adr, date, sort)
 
-city_table = create_table_city(city)
+            # Пауза для загрузки данных
+            time.sleep(0.5)
 
-# URL сайта
-url = 'https://4brewers.ru/'
-driver.get(url)
+            # вынести
+            soup = self.get_soup()
 
-page_source = driver.page_source
+            # Извлечение данных с помощью Супа
+            process_beer(soup, self.city_table, beer_title, sort)
 
-soup = BeautifulSoup(page_source, 'html.parser')
+    def run(self):
+        start_time = time.time()
 
-# Поиск всех элементов с классом 'productItem'
-beers = soup.find_all(class_='productItem')
+        self.driver.get(self.url)
 
-for beer in beers:
-    name = beer.find(class_='productItem__name').text.strip()
-    find_beer(name, beer)
+        soup = self.get_soup()
 
-# csv_it(city_table)
-# Закрытие драйвера
-driver.quit()
-end_time = time.time()
-print(f'Выполнили код за {end_time - start_time:.2f} секунд')
+        # Поиск всех элементов с классом 'productItem'
+        beer_cards = soup.find_all(class_='productItem')
+
+        for beer_card in beer_cards:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'productItem__name'))
+            )
+            beer_element_name = beer_card.find(class_='productItem__name').text.strip()
+            self.process_page(beer_element_name, beer_card)
+
+        self.driver.quit()
+        end_time = time.time()
+        print(f'Выполнили код за {end_time - start_time:.2f} секунд')
+
+
+def main():
+    p4b = Parse4Brewers('Воронеж')
+    p4b.run()
+
+
+if __name__ == '__main__':
+    main()
--- sql.py
+++ sql.py
@@ -3,12 +3,32 @@ from datetime import datetime
 from sqlalchemy import create_engine, UniqueConstraint
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String, Date
-from sqlalchemy import insert
+from sqlalchemy import insert, select, and_, delete
 import csv
 
 engine = create_engine("sqlite+pysqlite:///beer.db", echo=True)
 metadata_obj = MetaData()
 
+changes_table = Table(
+    'Изменения',
+    metadata_obj,
+Column('id', Integer, primary_key=True),
+    Column('name', String(60)),
+    Column('address', String(60)),
+    Column('prev_arr_date', Date),
+    Column('new_arr_date', Date),
+    Column('city', String(60))
+)
+
+def insert_changes(beer_name, beer_adr, prev_arr_date, new_arr_date, city, changes_table=changes_table):
+    insert_stmt = insert(changes_table).values(
+        name=beer_name,
+        address=beer_adr,
+        prev_arr_date=prev_arr_date,
+        new_arr_date=new_arr_date,
+        city=city
+    )
+    return insert_stmt
 
 def create_table_city(city):
     beer_table = Table(
@@ -19,18 +39,60 @@ def create_table_city(city):
         Column('address', String(60)),
         Column('last_arr_time', Date),
         Column('cur_date', Date),
-        UniqueConstraint('name', 'address', 'last_arr_time', 'cur_date')
+        Column('sort', String(60)),
+        UniqueConstraint('name', 'address', 'last_arr_time', 'cur_date', 'sort')
     )
     metadata_obj.create_all(engine)
     return beer_table
 
 
-def insert_beer(beer_table, beer_name, beer_adr, beer_arr_time, cur_date=datetime.today()):
-    cmd = insert(beer_table).prefix_with('OR IGNORE').values(name=beer_name, address=beer_adr,
-                                                             last_arr_time=beer_arr_time, cur_date=cur_date)
+def insert_beer(beer_table, beer_name, beer_adr, beer_arr_time, sort, cur_date=datetime.today()):
     with engine.connect() as conn:
-        conn.execute(cmd)
+        cmd = select(beer_table.c.last_arr_time).where(
+            and_(
+                beer_table.c.name == beer_name, beer_table.c.address == beer_adr, beer_table.c.sort == sort
+            )
+        )
+        res = conn.execute(cmd).fetchone()
+        if res:
+            prev_arr_time = res[0]
+            if prev_arr_time == beer_arr_time:
+                return
+            else:
+                delete_stmt = delete(beer_table).where(
+                    and_(
+                        beer_table.c.name == beer_name,
+                        beer_table.c.address == beer_adr,
+                        beer_table.c.sort == sort
+                    )
+                )
+                conn.execute(delete_stmt)
+
+                insert_stmt = insert(beer_table).values(
+                    name=beer_name,
+                    address=beer_adr,
+                    last_arr_time=beer_arr_time,
+                    cur_date=cur_date,
+                    sort=sort
+                )
+                conn.execute(insert_stmt)
+                #with open(f"changes/{datetime.today().strftime('%Y-%m-%d_')}{beer_name}_beer.csv", 'w', newline='', encoding='utf-8') as csvfile:
+                    #writer = csv.writer(csvfile)
+                    #writer.writerow(['name', 'address', 'prev_arr_time', 'new_arr_time'])
+                    #writer.writerow([beer_name, beer_adr, new_arr_time, beer_arr_time])
+                conn.execute(insert_changes(beer_name, beer_adr, prev_arr_time, beer_arr_time, f'{beer_table}'))
+
+        else:
+            insert_stmt = insert(beer_table).values(
+                name=beer_name,
+                address=beer_adr,
+                last_arr_time=beer_arr_time,
+                cur_date=cur_date,
+                sort=sort
+            )
+            conn.execute(insert_stmt)
         conn.commit()
+
 
 
 def csv_it(table):
